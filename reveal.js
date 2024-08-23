8734b2ed92f13b27a67ce5bae54c1c493bb18b6e
f0gel <f0gel@proton.me>
2024-08-23 16:04:33 +0200

    A lot.

--- reveal.js
+++ reveal.js
@@ -10,10 +10,38 @@ setTimeout(function() {
     toggle_visibility('showMe');
 }, 5000);
 
+setTimeout(function() {
+  toggle_visibility('showMore');
+}, 8000);
+
 fetch("code_resources/reveal.txt")
-  .then((res) => res.text())
-  .then((text) => {
-    console.log(text);
-    document.getElementById("thisProject").innerHTML=text;
-   })
-  .catch((e) => console.error(e));
+.then((res) => res.text())
+.then((text) => {
+  console.log(text);
+  document.getElementById("thisProject").innerHTML=text;
+  })
+.catch((e) => console.error(e));
+
+fetch("code_resources/Driver.txt")
+.then((res) => res.text())
+.then((text) => {
+  console.log(text);
+  document.getElementById("windowsDriver").innerHTML=text;
+  })
+.catch((e) => console.error(e));
+
+fetch("code_resources/TestReference.txt")
+.then((res) => res.text())
+.then((text) => {
+  console.log(text);
+  document.getElementById("testingLanguage").innerHTML=text;
+})
+.catch((e) => console.error(e));
+
+fetch("code_resources/FlaskBackend.txt")
+.then((res) => res.text())
+.then((text) => {
+  console.log(text);
+  document.getElementById("flaskBackend").innerHTML=text;
+})
+.catch((e) => console.error(e));
